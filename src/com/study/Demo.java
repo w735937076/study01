37e8df706a3877eebaa7ebf6e0766092065d1827
Nick <735937076@qq.com>
2021-03-04 21:30:41 +0800

    first commit

---
+++ src/com/study/Demo.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @description:
+ * @author:WZL 2021/3/4 9:29 下午
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello nick");
+    }
+}
